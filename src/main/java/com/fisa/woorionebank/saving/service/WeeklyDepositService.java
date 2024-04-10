f5c6ea3bf50deaf7019d29a476b7e531a7c85ec6
Dumbveloper <qhdtjq4799@naver.com>
2024-04-10 23:42:00 +0900

    feat: 적금 자동이체 기능 구현(#40)
    적금 자동이체 기능을 구현하였습니다.
    자동이체 될때 SavingHistory (입금내역)이 생기도록 했습니다.
    #40

--- src/main/java/com/fisa/woorionebank/saving/service/WeeklyDepositService.java
+++ src/main/java/com/fisa/woorionebank/saving/service/WeeklyDepositService.java
@@ -1,10 +1,7 @@
 package com.fisa.woorionebank.saving.service;
 
-import com.fisa.woorionebank.account.entity.Account;
 import com.fisa.woorionebank.common.execption.CustomException;
-import com.fisa.woorionebank.common.execption.ErrorCode;
 import com.fisa.woorionebank.saving.domain.entity.Saving;
-import com.fisa.woorionebank.saving.domain.responsedto.SavingDTO;
 import com.fisa.woorionebank.saving.repository.saving.SavingRepository;
 import com.fisa.woorionebank.savinghistory.entity.SavingHistory;
 import com.fisa.woorionebank.savinghistory.entity.TransactionType;
@@ -28,28 +25,28 @@ public class WeeklyDepositService {
 
     private final SavingRepository savingRepository;
     private final SavingHistoryRepository savingHistoryRepository;
+    private static final BigDecimal cost = BigDecimal.valueOf(10000);
 
     /**
      * 매주 10000원씩 입금 (매일 아침 9시에 벌크연산)
+     *
      * @param
      * @return
      */
-    @Scheduled(cron = "0 0 9 * * *") // 매일 아침 9시에 실행
+    @Scheduled(cron = "0 0 9 * * *")
     public void weeklyDeposit() {
 
-        // 오늘 요일 계산
+        // 메소드가 실행되는 요일 계산
         DayOfWeek today = LocalDateTime.now().getDayOfWeek();
 
         // 해당 요일에 생성되었고 생성된지 5일 이상 and 주차가 26주 이하인 적금 조회
         List<Saving> results = savingRepository.findSavingByCreatedDayOfweek(today);
 
-        BigDecimal cost = BigDecimal.valueOf(10000);
-
         for (Saving saving : results) {
             // 요일을 하루씩 update
             saving.updateCurrentWeek(1);
             try {
-                saving.getAccount().minus(cost);
+                autoDeposit(saving);
             } catch (CustomException e) {
                 //첫 연체 주 설정
                 if (saving.getOverdueWeek() == 0) {
@@ -57,8 +54,41 @@ public class WeeklyDepositService {
                 }
             }
         }
+    }
 
+    private void autoDeposit(Saving saving) {
 
+        // 연동 계좌에서 10,000원 차감
+        saving.getAccount().minus(cost);
 
+        // 적금 totalAmount 에 10,000원 추가
+        saving.plus(cost);
+
+        // 적금 입금 내역 기록 (자동이체 내역 기록)
+        savingHistoryRepository.save(SavingHistory.of(
+                TransactionType.CREATION,
+                cost,
+                saving
+        ));
     }
 }
+
+/**
+ * 생성된 요일로부터 일주일 후 부터 매주 해당 요일 09:00시에 10,000씩 자동 이체 합니다.
+ * Saving에 int overdueWeek, currentWeek 를 두고 연체된 주, 현재 주차를 기록합니다.
+ * Saving이 생성될때
+ *      overdueWeek = 0 : 연체된적 없음을 나타냅니다.
+ *      currentWeek = 1 : 생성비용(10,000)이 있기 때문에 첫째주 자동이체는 성공한 것으로 취급합니다.
+ * currentWeek는 매주 +1씩 update 됩니다.
+ * 통장에 잔고가 없어 연체가 발생하면 overdueWeek는 currentWeek로 기록 됩니다. (연체된 주 기록 : 우대금리를 계산하기 위함)
+ *
+ *
+ * 메소드 실행 프로세스
+ * 1. 현재 요일 계산: LocalDateTime.now().getDayOfWeek()를 사용하여 현재 요일을 계산
+ * 2. 조건에 맞는 적금 조회: 현재 요일에 생성되었고, 생성된 지 5일 이상이며, 주차가 26주 이하인 적금 계좌를 조회합니다.
+ * 3. 적금 계좌 처리:
+ *                 각 적금 계좌에 대해 현재 주차를 1주 증가시킵니다.
+ *                 연동된 계좌에서 10,000원을 차감합니다. 차감 중 예외가 발생하면 첫 연체 주를 설정합니다.
+ *
+ */
+
