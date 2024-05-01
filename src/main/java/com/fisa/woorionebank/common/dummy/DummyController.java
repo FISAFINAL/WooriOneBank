23c5cc8060b993fce27c77452863514753b68a12
parfait2 <parkyl0208@naver.com>
2024-05-01 17:51:42 +0900

    fix: dummy setting

---
+++ src/main/java/com/fisa/woorionebank/common/dummy/DummyController.java
@@ -0,0 +1,20 @@
+package com.fisa.woorionebank.common.dummy;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dummy")
+@RequiredArgsConstructor
+@Tag(name = "더미 API", description = "더미 데이터 insert")
+public class DummyController {
+    private final DummyService dummyService;
+
+    @GetMapping("/dummy")
+    public void test() {
+        dummyService.initData();
+    }
+}
