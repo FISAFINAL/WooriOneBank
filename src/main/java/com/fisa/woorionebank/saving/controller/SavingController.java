c4c36359b3fe496739414ef43bb231beddb92012
Dumbveloper <qhdtjq4799@naver.com>
2024-05-01 16:56:41 +0900

    fix: 컨트롤러에 로그 기록
    모니터링을 위한 로그 생성

--- src/main/java/com/fisa/woorionebank/saving/controller/SavingController.java
+++ src/main/java/com/fisa/woorionebank/saving/controller/SavingController.java
@@ -29,51 +29,55 @@ public class SavingController {
     @Operation(summary = "최애적금 생성", description = "회원이 최애적금을 생성합니다.")
     @PostMapping
     public ResponseEntity<SavingDTO> createSaving(@RequestBody SavingCreateRequestDTO requestDTO) {
+        log.info("적금 생성 호출");
         return ResponseEntity.ok(savingService.createSaving(requestDTO));
     }
 
     @Operation(summary = "최애적금 조회", description = "회원이 생성한 최애적금 리스트를 조회합니다.")
     @GetMapping("/{memberId}")
     public ResponseEntity<SavingListDTO> findSavings(@PathVariable("memberId") Long memberId) {
-        log.info("회원 적금 조회");
+        log.info("적금 조회 호출");
         return ResponseEntity.ok(savingService.findSavings(memberId));
     }
     @Operation(summary = "최애적금 조회", description = "회원이 생성한 최애적금 리스트를 조회합니다.")
     @GetMapping()
     public ResponseEntity<SavingListDTO> findSaving(@AuthenticationPrincipal Member member) {
-        System.out.println("controller : " + member.getMemberId());
-
+        log.info("적금 조회 호출");
         return ResponseEntity.ok(savingService.findSaving(member));
     }
 
     @Operation(summary = "최애적금 규칙 추가", description = "선택한 최애적금에 규칙이름과 규칙 비용을 추가합니다.")
     @PostMapping("/rules")
     public ResponseEntity<SavingRuleDTO> addRule(@RequestBody SavingAddRuleRequestDTO requestDTO) {
+        log.info("적금 규칙 추가 호출");
         return ResponseEntity.ok(savingService.addRule(requestDTO));
     }
 
     @Operation(summary = "최애적금 규칙 조회", description = "선택한 최애적금의 규칙 리스트를 조회합니다.")
     @GetMapping("/rules/{savingId}")
     public ResponseEntity<RuleListDTO> findRules(@PathVariable("savingId") Long savingId) {
-        System.out.println("/rules/{savingId} 호출");
+        log.info("적금 규칙 조회 호출");
         return ResponseEntity.ok(savingService.findRules(savingId));
     }
 
     @Operation(summary = "최애적금 입금", description = "규칙을 통해 연동된 계좌에서 최애적금으로 입금을 합니다.")
     @PostMapping("/deposit/{ruleId}")
     public ResponseEntity<SavingDTO> deposit(@PathVariable("ruleId") Long ruleId) {
+        log.info("적금 입금 호출");
         return ResponseEntity.ok(savingService.ruleDeposit(ruleId));
     }
 
     @Operation(summary = "최애적금 입금 내역 조회 (X)", description = "최애적금 입금 내역을 조회합니다.")
     @GetMapping("/history/{savingId}")
     public ResponseEntity<SavingHistoryListDTO> findHistory(@PathVariable("savingId") Long savingId) {
+        log.info("입금 내역 조회 호출");
         return ResponseEntity.ok(savingService.findHistory(savingId));
     }
 
     @Operation(summary = "최애적금 정보를 상세 조회합니다.", description = "최애적금 기본정보, 입금 내역, 현재금리를 조회합니다.")
     @GetMapping("/info/{savingId}")
     public ResponseEntity<SavingInfoDTO> savingInfo(@PathVariable("savingId") Long savingId) {
+        log.info("적금 상세 조회 호출");
         return ResponseEntity.ok(savingService.savingInfo(savingId));
     }
 
